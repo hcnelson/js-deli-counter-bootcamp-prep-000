41a25c53921809515f8a7f0a87648b906cb9ae10
Helen Nelson <hnelson@fandm.edu>
2021-05-26 12:59:47 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function takeANumber (katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(katzDeliLine) {
+  let i = 0
+  while (i < katzDeliLine.length) {
+    return `The line is currently: 
+  }
+}
